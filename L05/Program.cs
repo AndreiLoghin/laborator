1600cc3d46b12ae827c558f33634451e38ad33d2
AndreiLoghin <57018043+AndreiLoghin@users.noreply.github.com>
2021-11-15 01:41:02 +0200

    Add files via upload

---
+++ L05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var count = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Cycle no. " + count.ToString());
+                Console.WriteLine("Date: " + DateTime.Now.ToLocalTime());
+                count++;
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
